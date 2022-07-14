15a0e8aa92fa77692294a0b23a91099994844760
Artan <artanpl03@gmail.com>
2022-07-14 16:44:37 -0400

    made home,login,nav, productlist and signup component page

--- reveal/src/App.js
+++ reveal/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Navigation from './Navigation';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello</p>
+      <Navigation />
+      {/* <Home /> */}
     </div>
   );
 }
