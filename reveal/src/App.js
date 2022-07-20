7f6ebe22b1360560cac99dd173cdb98f77cee823
Artan <artanpl03@gmail.com>
2022-07-20 09:28:02 -0400

    changes to frontend

--- reveal/src/App.js
+++ reveal/src/App.js
@@ -1,14 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
-import Navigation from './Navigation';
+import { Route, Routes, Navigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Products from './Products';
+import Product from './Product';
+import Cart from './Cart';
+import Example from './Example';
+import CheckOut from './Checkout';
+
+
+
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  const handleAddProduct =  (product) => {
+    const ProductExist = products.find((item)=> item.id === product.id)
+    if (ProductExist) {
+      setProducts(
+        products.map((item) => 
+        item.id === product.id 
+        ? {...ProductExist, quantity: ProductExist.quantity + 1 } : item)
+      )
+    }
+    else{
+      setProducts([...products, {...product, quantity: 1}])
+    }
+  }
+
+
+  const getProduct = () => {
+    axios
+      .get('https://reveal-store-backend.herokuapp.com/products')
+      .then((res) => {
+        console.log(res.data);
+        
+        setProducts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(getProduct, []);
+
   return (
     <div className="App">
-      <Navigation />
+      {/* <Navigation /> */}
       {/* <Home /> */}
+      {/* <Products products={products}/> */}
+      <main>
+        <Routes>
+        <Route path="/" element={<Example products={products} handleAddProduct={handleAddProduct}/>} />
+        <Route path="/products" element={<Products products={products}/>}/>
+        
+       
+        <Route path="/cart" element={<Cart products={products} handleAddProduct={handleAddProduct}/>} />
+        <Route path="/product/:id" element={<Product products={products}/>}/>
+        
+        <Route path="/home" element={<Home products={products}/>} />
+        <Route path="/checkout" element={<CheckOut />} />
+        </Routes>
+      </main>
     </div>
+
+   
   );
 }
 
--- reveal/src/Home.js
+++ reveal/src/Home.js
@@ -1,11 +1,28 @@
 import React from "react";
 
-const Home = () =>{
+const Home = ({products}) =>{
 return(
-    <div>
-        Home
-    </div>
+    <div className="cart-items">
+        <div className="cart-items-header"> Cart Items</div>
 
+        {products.length === 0 && (<div> No items are added.</div>)}
+
+
+        <div>
+            {products.map((item) => (
+                <div key={item.id} className="cart-items-list">
+                    <img 
+                    className="cart-items-image"
+                    src={item.image} 
+                    alt={item.name}
+                    />
+                    {item.description}
+                    {item.name}
+                </div>
+            ))}
+        </div>
+    </div>
+    
     )
 
 }
